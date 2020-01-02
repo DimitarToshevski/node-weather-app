5bbed3834a15abf35b8fbe277fbc36030376780e
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-01-02 03:52:48 +0200

    Added mapbox request and getting lat and long

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const request = require('request');
 
-const url =
+const mapboxUrl =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic3JldGVuYSIsImEiOiJjazR3MjZ6bXMwMWRsM2xsa2Jtam5mNmpuIn0.GWbCsjLyn7LFCRqKTQlGlQ&language=bg&limit=1';
+
+const darkSkyUrl =
   'https://api.darksky.net/forecast/ce7b9c247dcad7fb05b8693c479f4882/37.8267,-122.4233?lang=bg&units=si';
 
-request({ url, json: true }, (err, res) => {
+request({ url: darkSkyUrl, json: true }, (err, res) => {
   console.log(
     res.body.daily.data[0].summary +
       'Температурата в момента навън е ' +
@@ -12,4 +15,12 @@ request({ url, json: true }, (err, res) => {
       res.body.currently.precipProbability +
       '% шанс за валежи.'
   );
+});
+
+request({ url: mapboxUrl, json: true }, (err, res) => {
+  const geoCoordinates = res.body.features[0].center;
+  const longitude = geoCoordinates[0];
+  const latitude = geoCoordinates[1];
+  console.log(longitude);
+  console.log(latitude);
 });
