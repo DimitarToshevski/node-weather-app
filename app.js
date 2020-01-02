7222cb1fa4394d6e5c0a5df35065ac1afe09b58c
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-01-02 18:13:56 +0200

    added error handling

--- app.js
+++ app.js
@@ -7,20 +7,34 @@ const darkSkyUrl =
   'https://api.darksky.net/forecast/ce7b9c247dcad7fb05b8693c479f4882/37.8267,-122.4233?lang=bg&units=si';
 
 request({ url: darkSkyUrl, json: true }, (err, res) => {
-  console.log(
-    res.body.daily.data[0].summary +
-      'Температурата в момента навън е ' +
-      res.body.currently.temperature +
-      ' градуса. Има ' +
-      res.body.currently.precipProbability +
-      '% шанс за валежи.'
-  );
+  if (err) {
+    console.log('Unable to connect to weather service.');
+  } else if (res.body.error) {
+    console.log('Unable to find location.');
+  } else {
+    console.log(
+      res.body.daily.data[0].summary +
+        'Температурата в момента навън е ' +
+        res.body.currently.temperature +
+        ' градуса. Има ' +
+        res.body.currently.precipProbability +
+        '% шанс за валежи.'
+    );
+  }
 });
 
 request({ url: mapboxUrl, json: true }, (err, res) => {
-  const geoCoordinates = res.body.features[0].center;
-  const longitude = geoCoordinates[0];
-  const latitude = geoCoordinates[1];
-  console.log(longitude);
-  console.log(latitude);
+  if (err) {
+    console.log('Unable to connect to location service');
+  } else if (res.body.features.length === 0) {
+    console.log(
+      'Unable to find location. Try again with different search term.'
+    );
+  } else {
+    const geoCoordinates = res.body.features[0].center;
+    const longitude = geoCoordinates[0];
+    const latitude = geoCoordinates[1];
+    console.log(longitude);
+    console.log(latitude);
+  }
 });
