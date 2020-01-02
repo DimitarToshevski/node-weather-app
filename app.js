8d27d0aafff359b223270065a3f87b4258bc178b
Dimitar Toshevski <dimitar.toshevski@mentormate.com>
2020-01-02 03:35:06 +0200

    parsing the response

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const request = require('request');
 
 const url =
-  'https://api.darksky.net/forecast/ce7b9c247dcad7fb05b8693c479f4882/37.8267,-122.4233';
+  'https://api.darksky.net/forecast/ce7b9c247dcad7fb05b8693c479f4882/37.8267,-122.4233?lang=bg&units=si';
 
-request({ url }, (err, res) => {
-  const data = JSON.parse(res.body);
-  console.log(data.currently);
+request({ url, json: true }, (err, res) => {
+  console.log(
+    res.body.daily.data[0].summary +
+      'Температурата в момента навън е ' +
+      res.body.currently.temperature +
+      ' градуса. Има ' +
+      res.body.currently.precipProbability +
+      '% шанс за валежи.'
+  );
 });
